1362686f564d5e95c4967e9eccb166ae24df7686
Mousa Mohammad Abdulmaxod <49567708+MousaMohammad@users.noreply.github.com>
2019-07-25 22:34:34 +0200

    Add files via upload

---
+++ Super_Market.py
@@ -0,0 +1,169 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "import mysql.connector"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 2,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "conn = mysql.connector.connect(host='localhost',user='root',passwd='',port=3306)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 3,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "cursor = conn.cursor()"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 4,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "cursor.execute('create database if not exists Super_Market;')"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 6,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "cursor.execute('use  Super_Market;')\n",
+    "cursor.execute('create table if not exists  items (id int primary key auto_increment,name varchar(30),price int  , quantity INT);')"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 5,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "cursor.execute('use  Super_Market;')\n",
+    "cursor.execute('drop table items;')"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 7,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "cursor.execute(\"INSERT INTO items VALUES(1,'cheese',20,60),(2,'milk',6,15),(3,'cake',120,3),(4,'bread',1,50);\")"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 8,
+   "metadata": {},
+   "outputs": [
+    {
+     "data": {
+      "text/plain": [
+       "[(1, 'cheese', 20, 60),\n",
+       " (2, 'milk', 6, 15),\n",
+       " (3, 'cake', 120, 3),\n",
+       " (4, 'bread', 1, 50)]"
+      ]
+     },
+     "execution_count": 8,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
+    "cursor.execute('SELECT * FROM items;')\n",
+    "cursor.fetchall()"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 9,
+   "metadata": {},
+   "outputs": [
+    {
+     "data": {
+      "text/plain": [
+       "[(1, 'cheese', 20, 60), (2, 'milk', 6, 15), (3, 'cake', 110, 4)]"
+      ]
+     },
+     "execution_count": 9,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
+    "cursor.execute(\"update items SET quantity = 4 , price = 110 WHERE NAME = 'cake';\")\n",
+    "cursor.execute('DELETE FROM items WHERE id = 4;')\n",
+    "cursor.execute('SELECT * FROM items;')\n",
+    "cursor.fetchall()"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 96,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "cursor.execute('SELECT COUNT(id) FROM Super_Market.items;')"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 10,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "cursor.execute('SELECT COUNT(id) FROM Super_Market.items WHERE price > 10;')"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.6.8"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
